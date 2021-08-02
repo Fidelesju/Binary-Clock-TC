47963e221d44f8400e175aed89581bd26bab4391
Julia Fideles <julia.hfideles@gmail.com>
2021-08-02 14:04:36 -0300

    Implementeção led's (Err: Não inicia o form)

--- src/main/java/com/totalcross/BinaryClockTC.java
+++ src/main/java/com/totalcross/BinaryClockTC.java
@@ -1,9 +1,6 @@
 package com.totalcross;
 import totalcross.ui.MainWindow;
-import totalcross.ui.gfx.Color;
-import totalcross.ui.Button;
 import totalcross.ui.ImageControl;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,8 +10,10 @@ import totalcross.sys.Settings;
 public class BinaryClockTC extends MainWindow {
 
 
-    Button btnMinutes1,btnMinutes2,btnMinutes3,btnMinutes4,btnMinutes5,btnMinutes6,btnHours1,btnHours2,btnHours3,
-    btnHours4,btnHours5,btnSeconds1,btnSeconds2,btnSeconds3,btnSeconds4,btnSeconds5,btnSeconds6;
+    private ImageControl imageControl, ledSeconds1,ledSeconds2,ledSeconds3,ledSeconds4,ledSeconds5,
+    ledSeconds6,ledMinutes1,ledMinutes2,ledMinutes3,ledMinutes4,ledMinutes5,ledMinutes6,ledHours1,ledHours2,ledHours3,
+    ledHours4,ledHours5;
+ 
 
     int seconds = 0, minutes = 1, hours = 1;
     int setSeconds,setMinutes,setHours;
@@ -23,96 +22,81 @@ public class BinaryClockTC extends MainWindow {
     boolean[] positionsHours = new boolean[5];
     String arrayHours[] = new String[3];
     String getHours,getMinutes,getSeconds;
-    private ImageControl imageControl;
+
+  
     public BinaryClockTC() {
         setUIStyle(Settings.MATERIAL_UI);
         setTitle("Binary Clock");
     }
 
     @Override
-    public void initUI() {
-        Images.loadImage();
+    public void initUI() { 
+         Images.loadImage();
 
         imageControl = new ImageControl(Images.background);
 
         imageControl.scaleToFit = true;
         imageControl.centerImage = true;
 
-        add(imageControl,CENTER,CENTER);
+        add(imageControl,CENTER,CENTER); 
 
         SetHours();
-        CreateButton();
+        CreateLeds();
+
         Thread thread = new Thread(()->{
             Score();   
           });
           thread.start(); 
- 
-          
           
     }
-    //Criando 
-    public void CreateButton(){
+
+    //Criando LED's
+    public void CreateLeds(){
+
+        Images.loadLed();
+        ledHours1 = new ImageControl(Images.ledDark);
+        ledHours2 = new ImageControl(Images.ledDark);
+        ledHours3 = new ImageControl(Images.ledDark);
+        ledHours4 = new ImageControl(Images.ledDark);
+        ledHours5 = new ImageControl(Images.ledDark);
+        ledMinutes1 = new ImageControl(Images.ledDark);
+        ledMinutes2 = new ImageControl(Images.ledDark);
+        ledMinutes3 = new ImageControl(Images.ledDark);
+        ledMinutes4 = new ImageControl(Images.ledDark);
+        ledMinutes5 = new ImageControl(Images.ledDark);
+        ledMinutes6 = new ImageControl(Images.ledDark);
+        ledSeconds1 = new ImageControl(Images.ledDark);
+        ledSeconds2 = new ImageControl(Images.ledDark);
+        ledSeconds3 = new ImageControl(Images.ledDark);
+        ledSeconds4 = new ImageControl(Images.ledDark);
+        ledSeconds5 = new ImageControl(Images.ledDark);
+        ledSeconds6 = new ImageControl(Images.ledDark);
+
 
         //Criando botões
-        btnHours1 = new Button("");
-        btnHours2 = new Button("");
-        btnHours3 = new Button("");
-        btnHours4 = new Button("");
-        btnHours5 = new Button("");
-        btnMinutes1 = new Button("");
-        btnMinutes2 = new Button("");
-        btnMinutes3 = new Button("");
-        btnMinutes4 = new Button("");
-        btnMinutes5 = new Button("");
-        btnMinutes6 = new Button("");
-        btnSeconds1 = new Button("");
-        btnSeconds2 = new Button("");
-        btnSeconds3 = new Button("");
-        btnSeconds4 = new Button("");
-        btnSeconds5 = new Button("");
-        btnSeconds6 = new Button("");
-    
-        //Adicionando ao Layout, posicionando e dimensionando tamanhos
-        add(btnHours1,LEFT + 140,TOP + 200,btnHours1.getPreferredWidth() >=  55  ? DP +  40: btnHours1.getPreferredWidth(),DP +  45);
-        add(btnHours2,LEFT + 190,TOP + 200,btnHours2.getPreferredWidth() >=  55  ? DP +  40: btnHours2.getPreferredWidth(),DP +  45);
-        add(btnHours3,LEFT + 240,TOP + 200,btnHours3.getPreferredWidth() >=  55  ? DP +  40: btnHours3.getPreferredWidth(),DP +  45);
-        add(btnHours4,LEFT + 290,TOP + 200,btnHours4.getPreferredWidth() >=  55  ? DP +  40: btnHours4.getPreferredWidth(),DP +  45);
-        add(btnHours5,LEFT + 340,TOP + 200,btnHours5.getPreferredWidth() >=  55  ? DP +  40: btnHours5.getPreferredWidth(),DP +  45);
+        add(ledHours1,LEFT + 140,TOP + 200);
+        add(ledHours2,LEFT + 190,TOP + 200);
+        add(ledHours3,LEFT + 240,TOP + 200);
+        add(ledHours4,LEFT + 290,TOP + 200);
+        add(ledHours5,LEFT + 340,TOP + 200);
 
-        add(btnMinutes1,LEFT + 140,TOP + 250,btnMinutes1.getPreferredWidth() >=  55  ? DP +  40: btnMinutes1.getPreferredWidth(),DP +  45);
-        add(btnMinutes2,LEFT + 190,TOP + 250,btnMinutes2.getPreferredWidth() >=  55  ? DP +  40: btnMinutes2.getPreferredWidth(),DP +  45);
-        add(btnMinutes3,LEFT + 240,TOP + 250,btnMinutes3.getPreferredWidth() >=  55  ? DP +  40: btnMinutes3.getPreferredWidth(),DP +  45);
-        add(btnMinutes4,LEFT + 290,TOP + 250,btnMinutes4.getPreferredWidth() >=  55  ? DP +  40: btnMinutes4.getPreferredWidth(),DP +  45);
-        add(btnMinutes5,LEFT + 340,TOP + 250,btnMinutes5.getPreferredWidth() >=  55  ? DP +  40: btnMinutes5.getPreferredWidth(),DP +  45);
-        add(btnMinutes6,LEFT + 390,TOP + 250,btnMinutes6.getPreferredWidth() >=  55  ? DP +  40: btnMinutes6.getPreferredWidth(),DP +  45);
+        add(ledMinutes1,LEFT + 140,TOP + 250);
+        add(ledMinutes2,LEFT + 190,TOP + 250);
+        add(ledMinutes3,LEFT + 240,TOP + 250);
+        add(ledMinutes4,LEFT + 290,TOP + 250);
+        add(ledMinutes5,LEFT + 340,TOP + 250);
+        add(ledMinutes6,LEFT + 390,TOP + 250);
 
-        add(btnSeconds1,LEFT + 140,TOP + 300,btnSeconds1.getPreferredWidth() >=  55  ? DP +  40: btnSeconds1.getPreferredWidth(),DP +  45);
-        add(btnSeconds2,LEFT + 190,TOP + 300,btnSeconds2.getPreferredWidth() >=  55  ? DP +  40: btnSeconds2.getPreferredWidth(),DP +  45);
-        add(btnSeconds3,LEFT + 240,TOP + 300,btnSeconds3.getPreferredWidth() >=  55  ? DP +  40: btnSeconds3.getPreferredWidth(),DP +  45);
-        add(btnSeconds4,LEFT + 290,TOP + 300,btnSeconds4.getPreferredWidth() >=  55  ? DP +  40: btnSeconds4.getPreferredWidth(),DP +  45);
-        add(btnSeconds5,LEFT + 340,TOP + 300,btnSeconds5.getPreferredWidth() >=  55  ? DP +  40: btnSeconds5.getPreferredWidth(),DP +  45);
-        add(btnSeconds6,LEFT + 390,TOP + 300,btnSeconds6.getPreferredWidth() >=  55  ? DP +  40: btnSeconds6.getPreferredWidth(),DP +  45);
-        
-        //Configurando cores padrão
-        btnHours1.setBackColor(Color.DARK);
-        btnHours2.setBackColor(Color.DARK);
-        btnHours3.setBackColor(Color.DARK);
-        btnHours4.setBackColor(Color.DARK);
-        btnHours5.setBackColor(Color.DARK);
-        btnMinutes1.setBackColor(Color.DARK);
-        btnMinutes2.setBackColor(Color.DARK);
-        btnMinutes3.setBackColor(Color.DARK);
-        btnMinutes4.setBackColor(Color.DARK);
-        btnMinutes5.setBackColor(Color.DARK);
-        btnMinutes6.setBackColor(Color.DARK);
-        btnSeconds1.setBackColor(Color.DARK);
-        btnSeconds2.setBackColor(Color.DARK);
-        btnSeconds3.setBackColor(Color.DARK);
-        btnSeconds4.setBackColor(Color.DARK);
-        btnSeconds5.setBackColor(Color.DARK);
-        btnSeconds6.setBackColor(Color.DARK);  
+        add(ledSeconds1,LEFT + 140,TOP + 300);
+        add(ledSeconds2,LEFT + 190,TOP + 300);
+        add(ledSeconds3,LEFT + 240,TOP + 300);
+        add(ledSeconds4,LEFT + 290,TOP + 300);
+        add(ledSeconds5,LEFT + 340,TOP + 300);
+        add(ledSeconds6,LEFT + 390,TOP + 300);
+
     }
     
+    //Pegando a hora do sistema
     public void SetHours(){
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss"); 
           getHours = sdf.format(new Date());
@@ -129,122 +113,126 @@ public class BinaryClockTC extends MainWindow {
 
           seconds = setSeconds;
           minutes = setMinutes;
-          hours = setHours;
+          hours = setHours;  
     }
     
+    //Verificando os segundos
     public void VerificationSeconds(){
         //Segundos casa 6
         if (positionsSeconds[0] == true) {
-            btnSeconds6.setBackColor(Color.RED);
+           ledSeconds6.setImage(Images.ledRed);
         } else{
-            btnSeconds6.setBackColor(Color.DARK);
+            ledSeconds6.setImage(Images.ledDark);
         }
         //Segundos casa 5
         if (positionsSeconds[1] == true) {
-            btnSeconds5.setBackColor(Color.RED);
+             ledSeconds5.setImage(Images.ledRed);
         } else{
-            btnSeconds5.setBackColor(Color.DARK);
+            ledSeconds5.setImage(Images.ledDark);
         }
         //Segundos casa 4
         if (positionsSeconds[2] == true) {
-            btnSeconds4.setBackColor(Color.RED);
+             ledSeconds4.setImage(Images.ledRed);
         } else{
-            btnSeconds4.setBackColor(Color.DARK);
+            ledSeconds4.setImage(Images.ledDark);
         }
         //Segundos casa 3
         if (positionsSeconds[3] == true) {
-            btnSeconds3.setBackColor(Color.RED);
+            ledSeconds3.setImage(Images.ledRed);
         } else{
-            btnSeconds3.setBackColor(Color.DARK);
+            ledSeconds3.setImage(Images.ledDark);
         }
         //Segundos casa 2
         if (positionsSeconds[4] == true) {
-            btnSeconds2.setBackColor(Color.RED);
+            ledSeconds2.setImage(Images.ledRed);
         } else{
-            btnSeconds2.setBackColor(Color.DARK);
+            ledSeconds2.setImage(Images.ledDark);
         }
         //Segundos casa 1
         if (positionsSeconds[5] == true) {
-            btnSeconds1.setBackColor(Color.RED);
+            ledSeconds1.setImage(Images.ledRed);
         } else{
-            btnSeconds1.setBackColor(Color.DARK);
+            ledSeconds1.setImage(Images.ledDark);
         }
         
     }
     
+    //Verificando os minutos
     public void VerificationMinutes(){
         //Minutos casa 6
         if (positionsMinutes[0] == true) {
-            btnMinutes6.setBackColor(Color.RED);
+            ledMinutes6.setImage(Images.ledRed);
         } else{
-            btnMinutes6.setBackColor(Color.DARK);
+            ledMinutes6.setImage(Images.ledDark);
         }
         //Minutos casa 5
         if (positionsMinutes[1] == true) {
-            btnMinutes5.setBackColor(Color.RED);
+             ledMinutes5.setImage(Images.ledRed);
         } else{
-            btnMinutes5.setBackColor(Color.DARK);
+            ledMinutes5.setImage(Images.ledDark);
         }
         //Minutos casa 4
         if (positionsMinutes[2] == true) {
-            btnMinutes4.setBackColor(Color.RED);
+             ledMinutes4.setImage(Images.ledRed);
         } else{
-            btnMinutes4.setBackColor(Color.DARK);
+            ledMinutes4.setImage(Images.ledDark);
         }
         //Minutos casa 3
         if (positionsMinutes[3] == true) {
-            btnMinutes3.setBackColor(Color.RED);
+             ledMinutes3.setImage(Images.ledRed);
         } else{
-            btnMinutes3.setBackColor(Color.DARK);
+            ledMinutes3.setImage(Images.ledDark);
         }
         //Minutos casa 2
         if (positionsMinutes[4] == true) {
-            btnMinutes2.setBackColor(Color.RED);
+             ledMinutes2.setImage(Images.ledRed);
         } else{
-            btnMinutes2.setBackColor(Color.DARK);
+            ledMinutes2.setImage(Images.ledDark);
         }
         //Minutos casa 1
         if (positionsMinutes[5] == true) {
-            btnMinutes1.setBackColor(Color.RED);
+             ledMinutes1.setImage(Images.ledRed);
         } else{
-            btnMinutes1.setBackColor(Color.DARK);
+            ledMinutes1.setImage(Images.ledDark);
         }    
     }
     
+    //Verificando as horas
     public void VerificationHours(){
         //Horas casa 5
         if (positionsHours[0] == true) {
-            btnHours5.setBackColor(Color.RED);
+            ledHours5.setImage(Images.ledRed);
         } else{
-            btnHours5.setBackColor(Color.DARK);
+            ledHours5.setImage(Images.ledDark);
         }
         //Horas casa 4
         if (positionsHours[1] == true) {
-            btnHours4.setBackColor(Color.RED);
+            ledHours4.setImage(Images.ledRed);
         } else{
-            btnHours4.setBackColor(Color.DARK);
+            ledHours4.setImage(Images.ledDark);
         }
         //Horas casa 3
         if (positionsHours[2] == true) {
-            btnHours3.setBackColor(Color.RED);
+            ledHours3.setImage(Images.ledRed);
         } else{
-            btnHours3.setBackColor(Color.DARK);
+            ledHours3.setImage(Images.ledDark);
         }
         //Horas casa 2
         if (positionsHours[3] == true) {
-            btnHours2.setBackColor(Color.RED);
+            ledHours2.setImage(Images.ledRed);
         } else{
-            btnHours2.setBackColor(Color.DARK);
+            ledHours2.setImage(Images.ledDark);
         }
         //Horas casa 1
         if (positionsHours[4] == true) {
-            btnHours1.setBackColor(Color.RED);
+            ledHours1.setImage(Images.ledRed);
         } else{
-            btnHours1.setBackColor(Color.DARK);
+            ledHours1.setImage(Images.ledDark);
         }
          
     }
-     
+    
+    //Contando as horas
     public void Score(){
 
         while (true) {   
@@ -274,7 +262,7 @@ public class BinaryClockTC extends MainWindow {
                     positionsMinutes[i] = (((minutes >> i)&0x1)==1);
                 }
                 for (int i = 5; i >=0; i--) {
-                  VerificationMinutes();
+                    VerificationMinutes();
                     System.out.print(positionsMinutes[i]?1:0);
                 }
                 System.out.println();
@@ -288,7 +276,7 @@ public class BinaryClockTC extends MainWindow {
                     positionsHours[i] = (((hours >> i)&0x1)==1);
                     }
                    for (int i = 0; i < 4; i++) {
-                       VerificationMinutes();
+                       VerificationHours();
                        System.out.print(positionsHours[i]?1:0);
                    }
                    System.out.println();
@@ -302,3 +290,4 @@ public class BinaryClockTC extends MainWindow {
         
     }
 }
+
--- src/main/java/com/totalcross/util/Images.java
+++ src/main/java/com/totalcross/util/Images.java
@@ -4,7 +4,7 @@ import totalcross.ui.dialog.MessageBox;
 import totalcross.ui.image.Image;
 
 public class Images {
-    public static Image background;
+    public static Image background,ledRed,ledDark;
 
     public Images(){
 
@@ -12,6 +12,15 @@ public class Images {
     public static void loadImage() {
         try {
             background = new Image("src/images/Binary-Clock.png");   
+        } catch (Exception e) {
+            MessageBox.showException(e, true);
+        }
+    }
+    public static void loadLed() {
+        try {
+            ledRed = new Image("src/Images/LedVermelho.jpg");    
+            ledDark = new Image("src/Images/LedDark.jpg"); 
+
         } catch (Exception e) {
             MessageBox.showException(e, true);
         }
